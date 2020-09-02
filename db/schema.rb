56b12622c02de375e3e37eae7353960e27fe083b
Audrey-Ella-xo <anitaudrey@gmail.com>
2020-09-02 18:24:10 +0100

    Add models for project, todo and items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_02_171504) do
+ActiveRecord::Schema.define(version: 2020_09_02_172238) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.boolean "done"
+    t.integer "todo_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["todo_id"], name: "index_items_on_todo_id"
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "created_by"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "todos", force: :cascade do |t|
+    t.string "title"
+    t.integer "progress"
+    t.integer "project_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["project_id"], name: "index_todos_on_project_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,4 +46,6 @@ ActiveRecord::Schema.define(version: 2020_09_02_171504) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "items", "todos"
+  add_foreign_key "todos", "projects"
 end
