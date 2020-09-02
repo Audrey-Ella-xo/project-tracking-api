0e042f9e63ff6ebc6f1cf6904a6c011a3a7842d8
Audrey-Ella-xo <anitaudrey@gmail.com>
2020-09-02 19:59:45 +0100

    Add controller REST actions and helpers

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,46 @@
 class TodosController < ApplicationController
+    before_action :set_project
+    before_action :set_project_todo, only: [:show, :update, :destroy]
+  
+    # GET /projects/:project_id/todos
+    def index
+      json_response(@project.todos)
+    end
+  
+    # GET /projects/:project_id/todos/:id
+    def show
+      json_response(@todo)
+    end
+  
+    # POST /projects/:project_id/todos
+    def create
+      @project.todos.create!(todo_params)
+      json_response(@project, :created)
+    end
+  
+    # PUT /projects/:project_id/todos/:id
+    def update
+      @todo.update(todo_params)
+      head :no_content
+    end
+  
+    # DELETE /projects/:project_id/todos/:id
+    def destroy
+      @todo.destroy
+      head :no_content
+    end
+  
+    private
+  
+    def todo_params
+      params.permit(:title, :progress)
+    end
+  
+    def set_project
+      @project = Project.find(params[:project_id])
+    end
+  
+    def set_project_todo
+      @todo = @project.todos.find_by!(id: params[:id]) if @project
+    end
 end
--- spec/factories/factories/projects.rb
+++ spec/factories/factories/projects.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
     factory :project do
       name { Faker::Lorem.character }
+      description { Faker::Lorem.character }
       created_by { Faker::Number.number(10) }
     end
 end
