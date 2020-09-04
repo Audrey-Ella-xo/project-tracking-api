6a52a94260b2a28b5fce681033c9abd90784312e
Audrey-Ella-xo <anitaudrey@gmail.com>
2020-09-04 19:54:57 +0100

    Add root toute

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   mount Raddocs::App => "/api_docs"
+  root to: 'projects#index'
   resources :projects do
     resources :todos do
       resources :items 
