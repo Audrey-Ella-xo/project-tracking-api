cdc96b3d94095477c72601de1d8fa58f6797018c
Audrey-Ella-xo <anitaudrey@gmail.com>
2020-09-05 17:26:32 +0100

    Add regex and uniqueness validation to email

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ApplicationRecord
   # Validations
   validates_presence_of :name, :email, :password_digest
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, format: {with: VALID_EMAIL_REGEX}, uniqueness: { case_sensitive: false}
+
   def to_admin
     update(admin: true)
   end
